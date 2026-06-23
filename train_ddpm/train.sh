6f98984e3b09ed2e6515fcfd8ffe8633050c58dc
Kaya Arai-Yokoi <kaya.araiyokoi@gmail.com>
2026-06-23 14:50:03 +0000

    Update train.sh 2

--- train_ddpm/train.sh
+++ train_ddpm/train.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=train_diffusion
-#SBATCH --partition=gpu          # (Change this to whatever partition your other script used)
+#SBATCH --partition=workq
 #SBATCH --nodes=1
-#SBATCH --gpus=1                 # Request 1 GPU
-#SBATCH --mem=32G                # Request plenty of RAM for your DataLoader
-#SBATCH --time=12:00:00          # Max time limit
-#SBATCH --output=train_%j.log    # Save output to this log file
+#SBATCH --gpus=1
+#SBATCH --mem=32G
+#SBATCH --time=12:00:00
+#SBATCH --output=train_%j.log
 
-export CUDA_VISIBLE_DEVICES=0;
+export CUDA_VISIBLE_DEVICES=0
 
+# Run the training script using our corrected configuration file
 python main.py --config configs/kmflow_re1000_rs256.yml --exp ./experiments/km256/ --doc ./weights/km256/ --ni
