7f640e12a4dbabd7b5f91f666192ea906abeb420
Kaya Arai-Yokoi <kaya.araiyokoi@gmail.com>
2026-06-23 15:51:46 +0000

    update

---
+++ run_inference_sparse_conditional.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=infer_sparse_cond
+#SBATCH --partition=workq
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --mem=32G
+#SBATCH --time=04:00:00
+#SBATCH --output=infer_sparse_cond_%j.log
+
+module load cray-python/3.11.7
+
+# Activate virtual environment
+source /scratch/u6ki/kayaay.u6ki/diffusion_env/bin/activate
+
+#Ensure we are in the main directory where kmflow_re1000_rs256.yml lives
+cd /scratch/u6ki/kayaay.u6ki/MSc-Project-Diffusion-Model
+
+
+python main.py --config configs/kmflow_re1000_rs256_sparse_recons_conditional.yml --seed 1234 --sample_step 1 --t 240 --r 30 --ni
