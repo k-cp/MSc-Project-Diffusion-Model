b85b1f31758f197ab61c8c49d475e95d4ec8bcc2
Kaya Arai-Yokoi <kaya.araiyokoi@gmail.com>
2026-06-23 12:05:29 +0000

    Add sbatch file

---
+++ run_inference.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=fluid_inference
+#SBATCH --partition=workq
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=01:00:00
+#SBATCH --output=inference_run_%j.log
+
+# 1. Load the python module
+module load cray-python/3.11.7
+
+# Activate virtual environment
+source /scratch/u6ki/kayaay.u6ki/diffusion_env/bin/activate
+
+#Ensure we are in the main directory where kmflow_re1000_rs256.yml lives
+cd /scratch/u6ki/kayaay.u6ki/MSc-Project-Diffusion-Model
+
+python main.py --config kmflow_re1000_rs256.yml --seed 1234 --sample_step 1 --t 240 --r 30
