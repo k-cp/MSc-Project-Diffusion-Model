e938d9dc38baca5d3e238e56f03be7653b611988
Kaya Arai-Yokoi <kaya.araiyokoi@gmail.com>
2026-07-13 15:10:32 +0000

    sh file

--- run_inference_conditional.sh
+++ run_inference_conditional.sh
@@ -1,28 +1,56 @@
 #!/bin/bash
+
 #SBATCH --job-name=fluid_dps
+
 #SBATCH --partition=workq
+
 #SBATCH --nodes=1
+
 #SBATCH --gpus=1
+
 #SBATCH --mem=32G
+
 #SBATCH --time=04:00:00
+
 #SBATCH --output=dps_recons_%j.log
+
+
+
 
 
 module load cray-python/3.11.7
 
 
+
+
+
 source /scratch/u6ki/kayaay.u6ki/diffusion_env/bin/activate
+
+
+
 
 
 cd /scratch/u6ki/kayaay.u6ki/MSc-Project-Diffusion-Model
 
 
+
+
+
 export ATEN_CPU_CAPABILITY=default
+
 export USE_MKLDNN=0
 
 
+
+
+
 python main.py --config kmflow_re1000_rs256_conditional.yml --seed 1234 \
+
     --run_dps 1 --operator sparse --zeta 0.3
+
+
+
+
 
 
 
