f6c07aae9cca3d176ba1b74997ef0073a72963ea
Kaya Arai-Yokoi <kaya.araiyokoi@gmail.com>
2026-07-13 15:55:48 +0100

    change sh

--- run_inference_conditional.sh
+++ run_inference_conditional.sh
@@ -22,4 +22,4 @@ export USE_MKLDNN=0
 
 
 python main.py --config kmflow_re1000_rs256_conditional.yml --seed 1234 \
-    --run_dps 1 --operator downsample --scale_factor 4 --zeta 0.3
+    --run_dps 1 --operator sparse --zeta 0.3
